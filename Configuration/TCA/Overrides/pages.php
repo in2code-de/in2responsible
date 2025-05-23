5c53903f198cb20cbc342916e4778d1a30068140
Alexander Kellner <alexander.kellner@in2code.de>
2025-05-23 10:14:36 +0200

    [TASK] Code cleanup

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,16 +1,17 @@
 <?php
 
+use TYPO3\CMS\Core\Domain\Repository\PageRepository;
+
 defined('TYPO3') || die();
 
-// add meta palette to page type 254 = sysfolder
-$GLOBALS['TCA']['pages']['types']['254']['showitem'] =
-    str_replace(
-        '--palette--;LLL:EXT:cms/locallang_tca.xlf:pages.palettes.visibility;hiddenonly,',
-        '--palette--;LLL:EXT:cms/locallang_tca.xlf:pages.palettes.visibility;hiddenonly, ' .
-        '--div--;LLL:EXT:cms/locallang_tca.xlf:pages.tabs.metadata, ' .
-        '--palette--;LLL:EXT:cms/locallang_tca.xlf:pages.palettes.editorial;editorial,',
-        $GLOBALS['TCA']['pages']['types']['254']['showitem']
-    );
+// add meta palette to sysfolders
+$GLOBALS['TCA']['pages']['types'][PageRepository::DOKTYPE_SYSFOLDER]['showitem'] = str_replace(
+    '--palette--;LLL:EXT:cms/locallang_tca.xlf:pages.palettes.visibility;hiddenonly,',
+    '--palette--;LLL:EXT:cms/locallang_tca.xlf:pages.palettes.visibility;hiddenonly, ' .
+    '--div--;LLL:EXT:cms/locallang_tca.xlf:pages.tabs.metadata, ' .
+    '--palette--;LLL:EXT:cms/locallang_tca.xlf:pages.palettes.editorial;editorial,',
+    $GLOBALS['TCA']['pages']['types'][PageRepository::DOKTYPE_SYSFOLDER]['showitem']
+);
 
 $tca = [
     'tx_in2responsible_check' => [
@@ -23,7 +24,7 @@ $tca = [
     ],
 ];
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $tca, 1);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $tca);
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
     'pages',
     'tx_in2responsible_check;;;;1-1-1',
